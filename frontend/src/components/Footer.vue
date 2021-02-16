cc5eaec1ce316135f1e120e88509a952b9112ec0
changshii-xen <siyamgoqi76@gmail.com>
2021-02-16 12:04:09 +0200

    edits

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -30,6 +30,47 @@
         To find out more about how I am please feel free to look through my social media, links are provided above.
         <br> If you wish to send me and email right now you can do so below: 
       </v-card-text>
+      <form
+  action="https://formspree.io/f/mvovyben"
+  method="POST"
+>
+  <v-row
+  align="center"
+    justify="space-around"
+    no-gutters
+  >
+      <v-col
+          cols="12"
+          sm="3"
+          md="3"
+        >
+          <v-text-field
+            label="Your Email"
+            type="text" 
+            name="_replyto"
+            outlined
+          ></v-text-field>
+        </v-col>
+    <v-col
+        cols="12"
+        md="6"
+      >
+        <v-textarea
+          filled
+          name="message"
+          label="Message"
+        ></v-textarea>
+      </v-col>
+  </v-row>
+  
+  <br>
+    
+   
+
+  <!-- your other form fields go here -->
+
+  <button type="submit">Send</button>
+</form>
 
       <v-divider></v-divider>
 
